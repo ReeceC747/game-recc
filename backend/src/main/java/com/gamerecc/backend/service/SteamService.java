50fb5accf647da8bae4be25ed014b43651396ef4
Reece Cohen <reececohen747@gmail.com>
2026-08-01 18:28:16 -0400

    set up structure for building the url to get the steamapps list

--- backend/src/main/java/com/gamerecc/backend/service/SteamService.java
+++ backend/src/main/java/com/gamerecc/backend/service/SteamService.java
@@ -1,5 +1,7 @@
 package com.gamerecc.backend.service;
 
+import java.net.http.HttpClient;
+
 import org.springframework.stereotype.Service;
 
 import com.gamerecc.backend.config.SteamConfig;
@@ -7,12 +9,27 @@ import com.gamerecc.backend.config.SteamConfig;
 @Service
 public class SteamService 
 {
-    private final SteamConfig steamconfig;
+    private final SteamConfig steamConfig;
+    private final HttpClient httpClient;
 
-    public SteamService(SteamConfig steamconfig)
+    public SteamService(SteamConfig steamConfig)
     {
-        this.steamconfig = steamconfig;
+        this.steamConfig = steamConfig;
+        this.httpClient = HttpClient.newHttpClient();
 
         System.out.println("SteamService created");
+
+        getAppList();
+    }
+
+    public String getAppList()
+    {
+        String url = 
+        "https://partner.steam-api.com/IStoreService/GetAppList/v1/"
+        + "?key=" + steamConfig.getApiKey()
+        + "&max_results=10";
+
+        return "not implemented yet"; 
+
     }
 }
